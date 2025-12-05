1b96f38470180b5dd14b530f6d0acc127c114f52
Yaroslav <99623192+punkesta@users.noreply.github.com>
2025-12-05 16:34:12 +0200

    Laboratory 7. CI/CD

--- src/main/java/com/noname/weblabs/JsonExt.kt
+++ src/main/java/com/noname/weblabs/JsonExt.kt
@@ -1,7 +1,8 @@
 package com.noname.weblabs
 
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
-val gson = GsonBuilder().setPrettyPrinting().create()
+val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
 fun Any.toJson(): String = gson.toJson(this)
